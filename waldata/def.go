8fb7b2b63b9227476f93d851ded85b6e157e9eb0
Wilhelm Liao <williaml.twcp@gmail.com>
2025-01-21 16:47:06 +0800

    WIP: impl ExtractWal2JsonData

--- waldata/def.go
+++ waldata/def.go
@@ -1,5 +1,17 @@
 package waldata
 
+import "encoding/json"
+
 const (
 	TAG_KEY = "key"
 )
+
+func ExtractWal2JsonData(buf []byte) (Wal2JsonDataSet, error) {
+	var data Wal2JsonDataSet
+
+	err := json.Unmarshal(buf, &data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
